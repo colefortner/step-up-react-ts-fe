15b6f9969c45eb9f1201d8d32311fe1d1cd33384
Cole Fortner <sasha2@Coles-MacBook-Pro.local>
2023-02-03 00:11:08 -0500

    Set up basic data connection with api in Test Component

---
+++ src/Components/Test.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react"
+
+interface ITest {
+    title: string;
+}
+
+export const Test = () => {
+  const [data, setData] = useState<ITest[]>([])
+ 
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/api/v1/connection_tests`)
+            .then(response => response.json())
+            .then((respData) => {
+            console.log(respData);
+            setData(respData);
+        })
+            .catch((e) => {
+                console.error(`An error occurred: ${e}`)
+            });
+         } , []);
+
+
+        return (
+        <>
+        <div className="App">
+            {data[0].title}
+        </div>
+        </>
+        );
+
+}
